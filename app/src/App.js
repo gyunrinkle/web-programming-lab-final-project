690c495b79185707775fca8079acaafc4e35a1af
Gyunseo Lee <rbstj0311@naver.com>
2022-12-11 06:33:41 +0000

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   .vscode/settings.json
    	modified:   app/package-lock.json
    	modified:   app/package.json
    	modified:   app/public/index.html
    	modified:   app/src/App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,10 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Button variant="primary">Primary</Button>
     </div>
   );
 }
